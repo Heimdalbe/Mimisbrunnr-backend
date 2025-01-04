2490b809df2babdf0fc316d7621ac6dc9a125740
De Bock Benny <bdebock@protonmail.com>
2025-01-04 17:58:27 +0100

    Create event location object

---
+++ Mimmisbrunnr.Domain/Event/Location.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimmisbrunnr.Domain.Event
+{
+    public class Location
+    {
+        #region Fields
+        private Guid _id;
+        private string _title;
+        #endregion
+
+        #region Properties
+        private string _address;
+        public string Address { get => _address; set => _address = value; }
+
+        private string _city;
+        public string City { get => _city; set => _city = value; }
+        #endregion
+
+        public Location(string title, string address, string city) { 
+            _title = Guard.Against.NullOrWhiteSpace(title, nameof(title));
+            _address = Guard.Against.NullOrWhiteSpace(address, nameof(address));
+            _city = Guard.Against.NullOrWhiteSpace(city, nameof(city));
+        }
+    }
+}
