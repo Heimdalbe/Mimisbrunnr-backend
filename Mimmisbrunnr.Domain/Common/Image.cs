db1d4ad4909c196e60d3bd903ba3c12a58c61856
De Bock Benny <bdebock@protonmail.com>
2025-01-04 16:11:10 +0100

    Create Image class for banners/albums

---
+++ Mimmisbrunnr.Domain/Common/Image.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimmisbrunnr.Domain.Common
+{
+    public class Image
+    {
+        #region Fields
+        private string _url;
+        #endregion
+
+        #region Properties
+        private string? _description;
+        public string Description { 
+            get => _description ?? ""; 
+            set => _description = Guard.Against.Null(value, nameof(Description)); 
+        }
+        #endregion
+
+        #region Constructors
+        public Image(string url, string description) : this(url)
+        {
+            Description = description;
+        }
+
+        public Image(string url)
+        {
+            Guard.Against.NullOrWhiteSpace(url, nameof(url));
+            _url = Guard.Against.Url(url, nameof(url));
+        }
+        #endregion
+    }
+}
