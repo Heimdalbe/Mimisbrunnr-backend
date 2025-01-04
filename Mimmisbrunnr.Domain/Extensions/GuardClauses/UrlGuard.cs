492cbb59113840df47a66bed51aed274b4d26e7e
De Bock Benny <bdebock@protonmail.com>
2025-01-04 16:12:19 +0100

    Add custom UrlGuard

---
+++ Mimmisbrunnr.Domain/Extensions/GuardClauses/UrlGuard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ardalis.GuardClauses
+{
+    public static class UrlGuard
+    {
+        public static string Url(this IGuardClause guardClause, string input,
+            [CallerArgumentExpression("input")] string? parameterName = null)
+        {
+            var urlPattern = @"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?";
+
+            if (!Regex.IsMatch(input, urlPattern))
+                throw new ArgumentException("Not a valid URL", parameterName);
+
+            return input;
+        }
+    }
+}
