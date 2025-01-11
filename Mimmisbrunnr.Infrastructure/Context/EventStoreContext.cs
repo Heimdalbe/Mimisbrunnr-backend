ebc779606129f8652253e908522e9ea9210e1b25
De Bock Benny <bdebock@protonmail.com>
2025-01-11 17:09:42 +0100

    Create EventDBContext

---
+++ Mimmisbrunnr.Infrastructure/Context/EventStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mimmisbrunnr.Domain.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimmisbrunnr.Infrastructure.Context
+{
+    public class EventStoreContext : DbContext
+    {
+        public EventStoreContext(DbContextOptions<EventStoreContext> options): base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.Location)
+                .WithMany(l => l.Events);
+        }
+    }
+}
