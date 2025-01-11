0ce54f0e08fd99d9823ef50df4b00296975ca531
De Bock Benny <bdebock@protonmail.com>
2025-01-11 17:09:31 +0100

    Create eventService

---
+++ Mimmisbrunnr.Infrastructure/Services/EventService.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.GuardClauses;
+using Microsoft.EntityFrameworkCore;
+using Mimmisbrunnr.Domain.Event;
+using Mimmisbrunnr.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace Mimmisbrunnr.Infrastructure.Services
+{
+    public class EventService: IService
+    {
+        private readonly EventStoreContext _eventStoreContext;
+
+        public EventService(EventStoreContext eventStoreContext)
+        {
+            _eventStoreContext = Guard.Against.Null(eventStoreContext, nameof(eventStoreContext));
+        }
+
+        public async Task<IEnumerable<Event>> GetAllAsync<Event>()
+        {
+            return (IEnumerable<Event>) await _eventStoreContext.Locations.ToListAsync();
+        }
+
+
+        public Task<T> Delete<T>(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById<T>(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update<T, U>(Guid id, U DTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mimmisbrunnr.Infrastructure/Services/IService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimmisbrunnr.Infrastructure.Services
+{
+    public interface IService
+    {
+        // COMMON GET METHODS
+        public Task<IEnumerable<T>> GetAllAsync<T>();
+        public Task<T> GetById<T>(Guid id);
+
+        public Task<T> Update<T, U>(Guid id, U DTO);
+
+        public Task<T> Delete<T>(Guid id);
+
+    }
+}
