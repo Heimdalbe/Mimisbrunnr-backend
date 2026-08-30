5db4159446252b6f5979abf14008315244e7736d
Wout Lesschaeve <wout.lesschaeve@student.hogent.be>
2026-08-30 18:51:20 +0200

    filters werken

--- src/Mimisbrunnr.Server/Endpoints/Events/GetPubEvents.cs
+++ src/Mimisbrunnr.Server/Endpoints/Events/GetPubEvents.cs
@@ -1,11 +1,9 @@
-using Mimisbrunnr.Services.Identity;
 using Mimisbrunnr.Shared.Common;
 using Mimisbrunnr.Shared.Events;
-using Mimisbrunnr.Shared.Identity;
 
 namespace Mimisbrunnr.Server.Endpoints.Events;
 
-public class GetPubEvents(IEventService eventService, ISessionContextProvider sessionContextProvider) : Endpoint<QueryRequest.SkipTake, Result<EventResponse.GetEvents>>
+public class GetPubEvents(IEventService eventService) : Endpoint<QueryRequest.SkipTake, Result<EventResponse.GetEvents>>
 {
     public override void Configure()
     {
@@ -15,10 +13,6 @@ public class GetPubEvents(IEventService eventService, ISessionContextProvider se
 
     public override Task<Result<EventResponse.GetEvents>> ExecuteAsync(QueryRequest.SkipTake req, CancellationToken ct)
     {
-        var user = sessionContextProvider.User;
-        if (user is not null && (user.IsInRole(AppRoles.Schacht) || user.IsInRole(AppRoles.Commilitones))){
-                return eventService.GetPublishedEvents(req, ct);
-        }
-        return eventService.GetOpenEvents(req, ct);
+        return eventService.GetPublishedEvents(req, ct);
     }
 }
